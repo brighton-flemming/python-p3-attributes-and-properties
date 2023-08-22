0db706242e60af33404e702ae85b8aa90b3b39b8
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-23 00:21:13 +0300

    Mission Passed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+      
+   def __init__(self, name=""):
+    self._name = ""
+    self.name = name
+     
+   @property
+   def name(self):
+      return self._name 
+
+   @name.setter
+   def name(self, another_name):
+      if (isinstance(another_name, str) and 1 <= len(another_name) <= 25):
+         self._name = another_name
+      else:
+         print("Name must be string between 1 and 25 characters.")
+         self._name = ""
+    
+pluto = Dog()
+pluto.name = "Randy"
+print(pluto.name)
+
+
+
+      
+
